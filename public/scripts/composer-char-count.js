8d49ef8a1df78709a5cf6ec00f0358344be53a58
Likai Liu <likailiu2@gmail.com>
2022-12-08 07:20:14 +0800

    Implement a event handler that tracks the character count

---
+++ public/scripts/composer-char-count.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    const charsLeft = 140 - $(this).val().length;
+    const counter = $(this).next().children(".counter");
+    counter.text(charsLeft);
+    if (charsLeft < 0) {
+      counter.css({"color": "red"});
+    } else {
+      counter.css({"color": "inherit"});
+    }
+  });
+});
