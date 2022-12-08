686590199a035575790c8f5e5404419a5f6fd10d
Likai Liu <likailiu2@gmail.com>
2022-12-09 00:19:01 +0800

    Implemenmt a function that takes a tweet object and returns a tweet article element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const createTweetElement = function(tweetObj) {
+  const $tweet = $(`<article class="tweet">${tweetObj.content.text}</article>`);
+  return $tweet;
+};
+
+const renderTweets = function(tweetsArr) {
+  for (let tweet of tweets)
+}
+const 
+$(document).ready(function() {
+  
+
+});
