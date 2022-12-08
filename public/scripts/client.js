0bc0ff2d24ffe41ed13973652339e631fb4d52a9
Likai Liu <likailiu2@gmail.com>
2022-12-09 03:57:17 +0800

    Implement a function loadTweets that fetches data and render the tweets upon success; add an event handler that prevents page original POST requests and replace it with an ajax request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,35 +2,9 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+*/
 
-
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
+// to be simplifed as the displayed time diff shouldn't be this detailed
 const getTimeDiff = function(timestamp) {
   const date = new Date(timestamp);
   const now = new Date();
@@ -82,6 +56,34 @@ const renderTweets = function(tweetsArr) {
   }
 };
 
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets/",
+    method: "GET"
+  })
+    .then((tweets) => {
+      renderTweets(tweets);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 $(document).ready(function() {
-  renderTweets(data);
+  loadTweets();
+  $("#new-tweet-form").submit(function(event) {
+    event.preventDefault();
+    const newTweetData = $(this).serialize();
+    $.ajax({
+      url: "/tweets/",
+      method: "POST",
+      data: newTweetData
+    })
+      .then(() => {
+        console.log("Tweet sent!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 });
